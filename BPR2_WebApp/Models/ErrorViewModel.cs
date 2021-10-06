908daf3466ec2051d43197cce207d57e60dc070f
karlich99 <33891069+karlajajic@users.noreply.github.com>
2021-10-06 14:16:16 +0200

    Add project files.

---
+++ BPR2_WebApp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace BPR2_WebApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
